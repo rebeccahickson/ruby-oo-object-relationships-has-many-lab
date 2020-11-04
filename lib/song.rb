77e4ec33641246d482f378daaefa73d7ef86aba3
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2020-11-04 15:42:02 -0500

    commit tree d0026e7ab60a7297529e003492cc0ec511582f73

---
+++ lib/song.rb
@@ -0,0 +1,20 @@
+require 'pry'
+
+class Song
+  attr_accessor :artist, :name, :genre
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def artist_name
+    artist ? artist.name : nil
+  end
+end
